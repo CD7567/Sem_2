24557285083dc97f828dce799618ecaeedf546bb
CD7567 <48135745+CD7567@users.noreply.github.com>
2022-03-05 21:50:08 +0300

    BigInteger fix

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <rational.h>
+#include <cstdint>
+
+#include <cstring>
 
 int main() {
-    std::cout << -INT32_MAX << std::endl;
-    Rational x = 5;        // дробь 5/1
-    std::cout << x << " ";        // вывод: 5
-    x.SetDenominator(15);  // дробь 1/3 (5/15 сократилась)
-    std::cout << x;        // вывод: 1/3
-    std::cin >> x;         // ввод: 14/-6, дробь: -7/3 (общий множитель 2; знаменатель > 0)
-    std::cout << x;        // вывод: 1/3
+    char str[30000]{'s'};
+
+    std::cout << strlen(str);
 }
