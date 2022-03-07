e8fdc37e7491108a785b7238ca0f6e1085374cc5
CD7567 <48135745+CD7567@users.noreply.github.com>
2022-03-07 23:29:22 +0300

    KthElement final

---
+++ contest_1/kth_element/kth_element.h
@@ -0,0 +1,49 @@
+#pragma once
+
+template <typename T>
+T* Partition(T* begin, T* end) {
+  T* it_left = begin;
+  T* it_right = end;
+  T pivot = *(begin + (end - begin) / 2);
+
+  while (it_left <= it_right) {
+    while (*it_left < pivot) {
+      ++it_left;
+    }
+
+    while (pivot < *it_right) {
+      --it_right;
+    }
+
+    if (it_left < it_right) {
+      T tmp = *it_left;
+      *it_left = *it_right;
+      *it_right = tmp;
+
+      it_left++;
+      it_right--;
+    } else {
+      break;
+    }
+  }
+
+  return it_right;
+}
+
+template <typename T>
+void KthElement(T* begin, T* kth_element, T* end) {
+  T* it_left = begin;
+  T* it_right = end - 1;
+
+  while (true) {
+    T* mid = Partition(it_left, it_right);
+
+    if (kth_element < mid) {
+      it_right = mid;
+    } else if (mid < kth_element) {
+      it_left = mid + 1;
+    } else {
+      break;
+    }
+  }
+}
