e9ba593475e156c9cb3308e90fefdc8d8469d64d
cd7567 <alexander.y.koshelev@gmail.com>
2022-03-23 19:10:32 +0300

    distance.cpp added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 struct Point {
   double x_, y_;
@@ -52,6 +53,43 @@ class Vector {
   double dim_x_, dim_y_;
 };
 
+double GetDistanceToLine(Point from, Point to, Point point) {
+  Vector vect_ab(from, to), vect_ac(from, point);
+
+  return std::abs(Vector::VectorMultiply(vect_ab, vect_ac)) / vect_ab.GetLength();
+}
+
+double GetDistanceToRay(Point from, Point to, Point point) {
+  Vector vect_ab(from, to), vect_ac(from, point), vect_bc(to, point);
+  double prod_ab_ac = Vector::ScalarMultiply(vect_ab, vect_ac);
+  double distance;
+
+  if (prod_ab_ac > 0) {
+    distance = std::abs(Vector::VectorMultiply(vect_ab, vect_ac)) / vect_ab.GetLength();
+  } else {
+    distance = vect_ac.GetLength();
+  }
+
+  return distance;
+}
+
+double GetDistanceToSection(Point from, Point to, Point point) {
+  Vector vect_ab(from, to), vect_ac(from, point), vect_bc(to, point);
+  double prod_ab_ac = Vector::ScalarMultiply(vect_ab, vect_ac);
+  double prod_ab_bc = Vector::ScalarMultiply(vect_ab, vect_bc);
+  double distance;
+
+  if (prod_ab_ac > 0 && prod_ab_bc < 0) {
+    distance = std::abs(Vector::VectorMultiply(vect_ab, vect_ac)) / vect_ab.GetLength();
+  } else if (prod_ab_ac <= 0) {
+    distance = vect_ac.GetLength();
+  } else if (prod_ab_bc >= 0) {
+    distance = vect_bc.GetLength();
+  }
+
+  return distance;
+}
+
 int main() {
   Point p_a, p_b, p_c;
 
@@ -59,21 +97,8 @@ int main() {
 
   Vector vect_ab(p_a, p_b), vect_ac(p_a, p_c), vect_bc(p_b, p_c);
 
-  if (vect_ac.IsParallel(vect_bc)) {
-    std::cout << "YES" << std::endl;
-  } else {
-    std::cout << "NO" << std::endl;
-  }
-
-  if (vect_ac.IsCodirectional(vect_ab)) {
-    std::cout << "YES" << std::endl;
-  } else {
-    std::cout << "NO" << std::endl;
-  }
-
-  if (vect_ac.IsOppositelyDirected(vect_bc)) {
-    std::cout << "YES" << std::endl;
-  } else {
-    std::cout << "NO" << std::endl;
-  }
+  std::cout << std::setprecision(9) << std::fixed;
+  std::cout << GetDistanceToLine(p_a, p_b, p_c) << std::endl;
+  std::cout << GetDistanceToRay(p_a, p_b, p_c) << std::endl;
+  std::cout << GetDistanceToSection(p_a, p_b, p_c) << std::endl;
 }
