c7a62e5a3751a1aee723ca533fa8247b679d7f56
cd7567 <alexander.y.koshelev@gmail.com>
2022-03-25 01:44:05 +0300

    distance.cpp added

--- contest_2/part_1/distance.cpp
+++ contest_2/part_1/distance.cpp
@@ -11,13 +11,10 @@ std::istream& operator>>(std::istream& in, Point& point) {
   return in;
 }
 
-bool operator==(const Point& lhs, const Point& rhs) {
-  return lhs.x_ == rhs.x_ && lhs.y_ == rhs.y_;
-}
-
 class Vector {
  public:
-  Vector(Point beg, Point end) : dim_x_(static_cast<double>(end.x_ - beg.x_)), dim_y_(static_cast<double>(end.y_ - beg.y_)){};
+  Vector(Point beg, Point end)
+          : dim_x_(static_cast<double>(end.x_ - beg.x_)), dim_y_(static_cast<double>(end.y_ - beg.y_)){};
   explicit Vector(double dim_x, double dim_y) : dim_x_(dim_x), dim_y_(dim_y){};
 
   [[nodiscard]] double GetLength() const {
@@ -36,15 +33,13 @@ class Vector {
   [[nodiscard]] bool IsParallel(const Vector& other) const {
     return dim_x_ * other.dim_y_ == other.dim_x_ * dim_y_;
   }
+
   [[nodiscard]] bool IsCodirectional(const Vector& other) const {
     return IsParallel(other) && (dim_x_ * other.dim_x_ >= 0) && (dim_y_ * other.dim_y_ >= 0);
   }
+
   [[nodiscard]] bool IsOppositelyDirected(const Vector& other) const {
     return IsParallel(other) && (dim_x_ * other.dim_x_ <= 0) && (dim_y_ * other.dim_y_ <= 0);
-  }
-
-  [[nodiscard]] bool IsZero() const {
-    return dim_x_ == 0 && dim_y_ == 0;
   }
 
   static double ScalarMultiply(const Vector& lhs, const Vector& rhs) {
@@ -59,34 +54,52 @@ class Vector {
   double dim_x_, dim_y_;
 };
 
-bool CheckBoxIntersect(double beg_1, double end_1, double beg_2, double end_2) {
-  if (beg_1 > end_1) {
-    std::swap(beg_1, end_1);
-  }
-  if (beg_2 > end_2) {
-    std::swap (beg_2, end_2);
-  }
+double GetDistanceToLine(Point from, Point to, Point point) {
+  Vector vect_ab(from, to), vect_ac(from, point);
 
-  return std::max(beg_1, beg_2) <= std::min(end_1, end_2);
+  return std::abs(Vector::VectorMultiply(vect_ab, vect_ac)) / vect_ab.GetLength();
 }
 
-bool DoSectionsIntersect(const Point& f_beg, const Point& f_end, const Point& s_beg, const Point& s_end) {
-  Vector vect_ab(f_beg, f_end), vect_cd(s_beg, s_end);
-  Vector vect_ac(f_beg, s_beg), vect_ad(f_beg, s_end), vect_bc(f_end, s_beg);
-  bool do_intersect = false;
+double GetDistanceToRay(Point from, Point to, Point point) {
+  Vector vect_ab(from, to), vect_ac(from, point), vect_bc(to, point);
+  double prod_ab_ac = Vector::ScalarMultiply(vect_ab, vect_ac);
+  double distance;
 
-  do_intersect = CheckBoxIntersect(f_beg.x_, f_end.x_, s_beg.x_, s_end.x_)
-                 && CheckBoxIntersect(f_beg.y_, f_end.y_, s_beg.y_, s_end.y_)
-                 && Vector::VectorMultiply(vect_ab, vect_ac) * Vector::VectorMultiply(vect_ab, vect_ad) <= 0
-                 && Vector::VectorMultiply(vect_cd, vect_ac) * Vector::VectorMultiply(vect_cd, vect_bc) <= 0;
+  if (prod_ab_ac > 0) {
+    distance = std::abs(Vector::VectorMultiply(vect_ab, vect_ac)) / vect_ab.GetLength();
+  } else {
+    distance = vect_ac.GetLength();
+  }
 
-  return do_intersect;
+  return distance;
+}
+
+double GetDistanceToSection(Point from, Point to, Point point) {
+  Vector vect_ab(from, to), vect_ac(from, point), vect_bc(to, point);
+  double prod_ab_ac = Vector::ScalarMultiply(vect_ab, vect_ac);
+  double prod_ab_bc = Vector::ScalarMultiply(vect_ab, vect_bc);
+  double distance;
+
+  if (prod_ab_ac > 0 && prod_ab_bc < 0) {
+    distance = std::abs(Vector::VectorMultiply(vect_ab, vect_ac)) / vect_ab.GetLength();
+  } else if (prod_ab_ac <= 0) {
+    distance = vect_ac.GetLength();
+  } else if (prod_ab_bc >= 0) {
+    distance = vect_bc.GetLength();
+  }
+
+  return distance;
 }
 
 int main() {
-  Point p_a, p_b, p_c, p_d;
+  Point p_a, p_b, p_c;
 
-  std::cin >> p_a >> p_b >> p_c >> p_d;
+  std::cin >> p_c >> p_a >> p_b;
 
-  std::cout << (DoSectionsIntersect(p_a, p_b, p_c, p_d) ? "YES" : "NO") << std::endl;
+  Vector vect_ab(p_a, p_b), vect_ac(p_a, p_c), vect_bc(p_b, p_c);
+
+  std::cout << std::setprecision(9) << std::fixed;
+  std::cout << GetDistanceToLine(p_a, p_b, p_c) << std::endl;
+  std::cout << GetDistanceToRay(p_a, p_b, p_c) << std::endl;
+  std::cout << GetDistanceToSection(p_a, p_b, p_c) << std::endl;
 }
