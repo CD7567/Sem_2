b16172fb4a64ff3e8058da0b8381c805497519a1
cd7567 <alexander.y.koshelev@gmail.com>
2022-03-28 11:29:25 +0300

    unique_ptr.h added

---
+++ contest_2/part_2/unique_ptr/unique_ptr.h
@@ -0,0 +1,60 @@
+#pragma once
+
+template <typename T>
+class UniquePtr {
+ public:
+  UniquePtr() : ptr_(nullptr){};
+  explicit UniquePtr(T* ptr) : ptr_(ptr){};
+  UniquePtr(const UniquePtr&) = delete;
+  UniquePtr(UniquePtr&& src) noexcept : ptr_(src.ptr_) {
+    src.ptr_ = nullptr;
+  }
+
+  ~UniquePtr() {
+    delete ptr_;
+  }
+
+  UniquePtr& operator=(const UniquePtr&) = delete;
+  UniquePtr& operator=(UniquePtr&& src) noexcept {
+    ptr_ = src.ptr_;
+    src.ptr_ = nullptr;
+
+    return *this;
+  };
+
+  T* Release() {
+    T* ptr = ptr_;
+    ptr_ = nullptr;
+
+    return ptr;
+  }
+  void Reset(T* new_ptr = nullptr) {
+    delete ptr_;
+
+    ptr_ = new_ptr;
+  }
+
+  void Swap(UniquePtr& other) {
+    T* tmp = std::move(ptr_);
+    ptr_ = std::move(other.ptr_);
+    other.ptr_ = std::move(tmp);
+  }
+
+  T* Get() const {
+    return ptr_;
+  }
+
+  T& operator*() const {
+    return *ptr_;
+  }
+  T* operator->() const {
+    return ptr_;
+  }
+
+  explicit operator bool() const {
+    return ptr_ != nullptr;
+  }
+
+ private:
+  T* ptr_;
+};
