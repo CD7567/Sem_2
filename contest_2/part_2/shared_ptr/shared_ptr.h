5e1398247a418e6cc01fc12de66eb8d51ad5f92e
cd7567 <alexander.y.koshelev@gmail.com>
2022-03-30 14:14:42 +0300

    shared_ptr.h pre weak commit

--- contest_2/part_2/shared_ptr/shared_ptr.h
+++ contest_2/part_2/shared_ptr/shared_ptr.h
@@ -2,50 +2,54 @@
 
 #include <cstdint>
 
+struct Counter {
+  size_t strong_count_, weak_count_;
+};
+
 template <typename T>
 class SharedPtr {
  public:
-  SharedPtr() : ptr_(nullptr), counter_(new size_t{}){};
-  SharedPtr(T* object_ptr) : ptr_(object_ptr), counter_{new size_t{1}} {  //  NOLINT
+  SharedPtr() : ptr_(nullptr), new_counter_(new Counter{}){};
+  SharedPtr(T* object_ptr) : ptr_(object_ptr), new_counter_{new Counter{1}} {  //  NOLINT
     if (object_ptr == nullptr) {
-      *counter_ = 0;
+      new_counter_->strong_count_ = 0;
     }
   };
-  SharedPtr(const SharedPtr& src) : ptr_(src.ptr_), counter_(src.counter_) {
-    if (*counter_ != 0) {
-      ++(*counter_);
+  SharedPtr(const SharedPtr& src) : ptr_(src.ptr_), new_counter_(src.new_counter_) {
+    if (new_counter_->strong_count_ != 0) {
+      ++new_counter_->strong_count_;
     } else {
-      counter_ = new size_t{};
+      new_counter_ = new Counter{};
     }
   }
-  SharedPtr(SharedPtr&& src) noexcept : ptr_(src.ptr_), counter_(src.counter_) {
+  SharedPtr(SharedPtr&& src) noexcept : ptr_(src.ptr_), new_counter_(src.new_counter_) {
     src.ptr_ = nullptr;
-    src.counter_ = new size_t{};
+    src.new_counter_ = new Counter{};
   }
 
   ~SharedPtr() {
-    if (*counter_ > 1) {
-      --(*counter_);
+    if (new_counter_->strong_count_ > 1) {
+      --(new_counter_->strong_count_);
     } else {
       delete ptr_;
-      delete counter_;
+      delete new_counter_;
     }
   }
 
   SharedPtr& operator=(const SharedPtr& src) {
     if (this != &src) {
-      if (*counter_ <= 1) {
+      if (new_counter_->strong_count_ <= 1) {
         delete ptr_;
-        delete counter_;
+        delete new_counter_;
       } else {
-        --(*counter_);
+        --new_counter_->strong_count_;
       }
 
       if (*src.counter_ == 0) {
-        counter_ = new size_t{};
+        new_counter_ = new Counter{};
       } else {
-        counter_ = src.counter_;
-        ++(*counter_);
+        new_counter_ = src.new_counter_;
+        ++new_counter_->strong_count_;
       }
 
       ptr_ = src.ptr_;
@@ -55,49 +59,49 @@ class SharedPtr {
   }
   SharedPtr& operator=(SharedPtr&& src) noexcept {
     if (this != &src) {
-      if (*counter_ <= 1) {
+      if (new_counter_->strong_count_ <= 1) {
         delete ptr_;
-        delete counter_;
+        delete new_counter_;
       } else {
-        --(*counter_);
+        --new_counter_->strong_count_;
       }
 
       ptr_ = src.ptr_;
-      counter_ = src.counter_;
+      new_counter_ = src.counter_;
 
       src.ptr_ = nullptr;
-      src.counter_ = new size_t{};
+      src.new_counter_ = new Counter{};
     }
 
     return *this;
   }
 
   void Reset(T* new_ptr = nullptr) {
-    if (*counter_ <= 1) {
+    if (new_counter_->strong_count_ <= 1) {
       delete ptr_;
-      delete counter_;
+      delete new_counter_;
     } else {
-      --(*counter_);
+      --new_counter_;
     }
 
     ptr_ = new_ptr;
-    counter_ = new size_t{};
+    new_counter_ = new Counter{};
 
     if (new_ptr != nullptr) {
-      ++(*counter_);
+      ++new_counter_->strong_count_;
     }
   }
 
   void Swap(SharedPtr& other) {
     if (this != &other) {
       T* tmp = std::move(ptr_);
-      size_t* tmp_counter = counter_;
+      Counter* tmp_counter = new_counter_;
 
       ptr_ = std::move(other.ptr_);
-      counter_ = std::move(other.counter_);
+      new_counter_ = other.new_counter_;
 
       other.ptr_ = std::move(tmp);
-      other.counter_ = tmp_counter;
+      other.new_counter_ = tmp_counter;
     }
   }
 
@@ -106,7 +110,7 @@ class SharedPtr {
   }
 
   [[nodiscard]] size_t UseCount() const {
-    return *counter_;
+    return new_counter_->strong_count_;
   }
 
   T& operator*() const {
@@ -122,5 +126,5 @@ class SharedPtr {
 
  private:
   T* ptr_;
-  size_t* counter_;
+  Counter* new_counter_;
 };
