7b83d1b284897dbe3ca0b94f5f1abd20f2f24cb7
cd7567 <alexander.y.koshelev@gmail.com>
2022-04-29 12:07:47 +0300

    enumerate.h ckeckpoint

---
+++ contest_3/part_2/iter/enumerate.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#define TEMPORARY_ENUMERATE_IMPLEMENTED
+
+#include <cstdint>
+#include <algorithm>
+
+template <typename Collection, typename CollIterator = std::conditional_t<std::is_const_v<Collection>, typename Collection::const_iterator, typename Collection::iterator>>
+class Enumerate {
+ public:
+  using ItValueType = std::pair<size_t, std::conditional_t<std::is_const_v<Collection>, typename Collection::const_reference, typename Collection::reference>>;
+
+ private:
+  class Iterator : public std::iterator<std::forward_iterator_tag, ItValueType, std::ptrdiff_t, ItValueType*, ItValueType&> {
+   public:
+    Iterator() = delete;
+    Iterator(const CollIterator& it, size_t idx) : idx_(idx), iterator_(it){};
+    Iterator(const Iterator&) = default;
+    Iterator(Iterator&&)  noexcept = default;
+
+    ~Iterator() = default;
+
+    Iterator& operator=(const Iterator&) = default;
+    Iterator& operator=(Iterator&&)  noexcept = default;
+
+    Iterator& operator++() {
+      ++idx_;
+      ++iterator_;
+      return *this;
+    }
+    Iterator operator++(int) {
+      auto last_iter = iterator_;
+      ++idx_;
+      ++iterator_;
+
+      return {last_iter, idx_ - 1};
+    }
+
+    ItValueType operator*() const {
+      return {idx_, *iterator_};
+    }
+
+    friend bool operator==(const Iterator& lhs, const Iterator& rhs) noexcept {
+      return lhs.idx_ == rhs.idx_;
+    }
+    friend bool operator!=(const Iterator& lhs, const Iterator& rhs) noexcept {
+      return !(lhs == rhs);
+    }
+    friend bool operator<(const Iterator& lhs, const Iterator& rhs) noexcept {
+      return lhs.idx_ < rhs.idx_;
+    }
+    friend bool operator>(const Iterator& lhs, const Iterator& rhs) noexcept {
+      return rhs < lhs;
+    }
+    friend bool operator<=(const Iterator& lhs, const Iterator& rhs) noexcept {
+      return !(rhs < lhs);
+    }
+    friend bool operator>=(const Iterator& lhs, const Iterator& rhs) noexcept {
+      return !(lhs < rhs);
+    }
+
+   private:
+    size_t idx_;
+    CollIterator iterator_;
+  };
+
+ public:
+  using iterator = Iterator;  //  NOLINT
+  using const_iterator = Iterator;  //  NOLINT
+  using reference = ItValueType&;  //  NOLINT
+  using const_reference = const ItValueType&;  //  NOLINT
+
+  Enumerate() = delete;
+  explicit Enumerate(Collection& coll) : temp_collection_(), begin_(coll.begin()), end_(coll.end()) {}
+  explicit Enumerate(Collection&& coll) : temp_collection_(coll), begin_(temp_collection_.begin()), end_(temp_collection_.end()) {}
+  Enumerate(const Enumerate&) = delete;
+  Enumerate(Enumerate&&) = delete;
+
+  ~Enumerate() = default;
+
+  Iterator begin() const {
+    //return {temp_collection_.begin(), 0};
+    return {begin_, 0};
+    //std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }
+
+  Iterator end() const {
+    //CollIterator&& it_begin = temp_collection_.begin();
+    //CollIterator&& it_end = temp_collection_.end();
+    return {end_, static_cast<size_t>(std::distance(begin_, end_))};
+  }
+
+ private:
+  Collection temp_collection_;
+  CollIterator begin_;
+  CollIterator end_;
+};
--- main.cpp
+++ main.cpp
@@ -3,47 +3,16 @@
 #include <vector>
 #include <typeinfo>
 #include <forward_list>
-#include "contest_3/part_2/vector/vector.h"
+#include "contest_3/part_2/iter/enumerate.h"
 
-template <typename Arg>
-void f(Arg&&) {
-  std::cout << __PRETTY_FUNCTION__ << '\n';
-};
-
-void g(const int&) {
-  std::cout << __PRETTY_FUNCTION__ << '\n';
-};
-
-void foo(int, int&);
-
-typedef const int val;
-typedef const val cval;
-
-static std::hash<std::basic_string<char>> hash1;
-
-void foo(const std::string& a) {
-  std::cout << hash1(a);
+template <typename T, typename It = typename T::iterator>
+void foo(T&) {
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
 int main() {
-  auto hash = std::hash<int>();
-  auto list = std::forward_list<int>();
+  auto p = Enumerate(std::vector<int>());
+  auto t = Enumerate(p);
 
-  const int a = 1;
-  const int& b = a;
-  const int&& c = 1;
-
-  f(1);
-  f(a);
-  f(b);
-  f(c);
-
-  g(a);
-  g(b);
-  g(c);
-  g(1);
-
-  const auto str = std::to_string(1);
-
-  foo(str);
+  foo(t);
 }
