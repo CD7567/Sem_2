48b0070567e2c2baa19c1d52ccedc668f95071fd
cd7567 <alexander.y.koshelev@gmail.com>
2022-04-30 18:48:36 +0300

    reversed.h checkpoint

--- contest_3/part_2/iter/reversed.h
+++ contest_3/part_2/iter/reversed.h
@@ -5,39 +5,45 @@
 #include <cstdint>
 #include <algorithm>
 
-template <typename Collection, typename CollIterator = std::conditional_t<std::is_const_v<Collection>, typename Collection::const_iterator, typename Collection::iterator>>
+template <typename Collection,
+          typename CollIterator = std::conditional_t<std::is_const_v<Collection>, typename Collection::const_iterator,
+                                                     typename Collection::iterator>>
 class Reversed {
  public:
-  using value_type = std::conditional_t<std::is_const_v<Collection>, const typename Collection::value_type, typename Collection::value_type>;  //  NOLINT
-  using reference = value_type&;  //  NOLINT
-  using const_reference = const value_type&;  //  NOLINT
+  using value_type = std::conditional_t<std::is_const_v<Collection>, const typename Collection::value_type,  //  NOLINT
+                                        typename Collection::value_type>;                                    //  NOLINT
+  using reference = value_type&;                                                                             //  NOLINT
+  using const_reference = const value_type&;                                                                 //  NOLINT
 
-  using iterator = std::reverse_iterator<CollIterator>;  //  NOLINT
+  using iterator = std::reverse_iterator<CollIterator>;                               //  NOLINT
   using const_iterator = std::reverse_iterator<typename Collection::const_iterator>;  //  NOLINT
-  using reversed_iterator = CollIterator;  //  NOLINT
-  using const_reversed_iterator = typename Collection::const_iterator;  //  NOLINT
+  using reversed_iterator = CollIterator;                                             //  NOLINT
+  using const_reversed_iterator = typename Collection::const_iterator;                //  NOLINT
 
   Reversed() = delete;
-  explicit Reversed(Collection& coll) : temp_collection_(), begin_(coll.begin()), end_(coll.end()) {}
-  explicit Reversed(Collection&& coll) : temp_collection_(coll), begin_(temp_collection_.begin()), end_(temp_collection_.end()) {}
+  explicit Reversed(Collection& coll) : temp_collection_(), begin_(coll.begin()), end_(coll.end()) {
+  }
+  explicit Reversed(Collection&& coll)
+      : temp_collection_(coll), begin_(temp_collection_.begin()), end_(temp_collection_.end()) {
+  }
   Reversed(Reversed&) = delete;
   Reversed(Reversed&&) = delete;
 
   ~Reversed() = default;
 
-  iterator begin() const {
+  iterator begin() const {  //  NOLINT
     return std::make_reverse_iterator(end_);
   }
 
-  iterator end() const {
+  iterator end() const {  //  NOLINT
     return std::make_reverse_iterator(begin_);
   }
 
-  reversed_iterator rbegin() const {
+  reversed_iterator rbegin() const {  //  NOLINT
     return begin_;
   }
 
-  reversed_iterator rend() const {
+  reversed_iterator rend() const {  //  NOLINT
     return end_;
   }
 
