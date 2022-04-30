9f529f0e3715cffb440a48ad0a94ffd82e24db88
cd7567 <alexander.y.koshelev@gmail.com>
2022-04-30 20:48:51 +0300

    unordered_set.h checkpoint

--- contest_3/part_2/unordered_set/unordered_set.h
+++ contest_3/part_2/unordered_set/unordered_set.h
@@ -6,71 +6,68 @@
 #include <functional>
 #include <vector>
 #include <forward_list>
+#include <iterator>
+
+template <typename KeyT, bool IsConst>
+struct Position {
+  std::conditional_t<IsConst, const std::forward_list<KeyT>&, std::forward_list<KeyT>&> bucket_;
+  typename std::forward_list<KeyT>::const_iterator pre_iterator_;
+};
 
 template <typename KeyT>
 class UnorderedSet {
  public:
-  UnorderedSet() : n_elements_(0) {};
-  explicit UnorderedSet(size_t count) : n_elements_(0) {
-    buckets_.reserve(count);
-
-    for (size_t i = 0; i < count; ++i) {
-      buckets_.push_back(std::forward_list<KeyT>());
-    }
-  };
-
+  UnorderedSet() : n_elements_(0){};
+  explicit UnorderedSet(size_t count) : n_elements_(0), buckets_({count}) {
+  }
   template <typename Iterator,
-          typename = std::enable_if_t<std::is_base_of_v<std::forward_iterator_tag,
-                  typename std::iterator_traits<Iterator>::iterator_category>>>
+            typename = std::enable_if_t<std::is_base_of_v<std::forward_iterator_tag,
+                                                          typename std::iterator_traits<Iterator>::iterator_category>>>
   UnorderedSet(Iterator first, Iterator last) : n_elements_(0) {
     for (auto it = first; it != last; ++it) {
-      this->Insert(*it);
+      Insert(*it);
     }
   }
-
-  UnorderedSet(const UnorderedSet<KeyT>& other) = default;
-  UnorderedSet(UnorderedSet&& src) noexcept {
-    buckets_ = std::move(src.buckets_);
-    n_elements_ = src.n_elements_;
+  UnorderedSet(const UnorderedSet& src) = default;
+  UnorderedSet(UnorderedSet&& src) noexcept : buckets_(std::move(src.buckets_)), n_elements_(src.n_elements_) {
     src.n_elements_ = 0;
   }
+
   ~UnorderedSet() = default;
 
-  UnorderedSet& operator=(const UnorderedSet&) = default;
+  UnorderedSet& operator=(const UnorderedSet& src) = default;
   UnorderedSet& operator=(UnorderedSet&& src) noexcept {
     buckets_ = std::move(src.buckets_);
     n_elements_ = src.n_elements_;
+
     src.n_elements_ = 0;
 
     return *this;
-  }
-
-  [[nodiscard]] size_t BucketCount() const {
-    return buckets_.size();
-  }
-  [[nodiscard]] size_t BucketSize(size_t id) const {
-    size_t size = 0;
-
-    if (buckets_.capacity() > id) {
-      for (auto it = buckets_[id].begin(); it != buckets_[id].end(); ++it, ++size) {
-      }
-    }
-
-    return size;
-  }
-
-  [[nodiscard]] size_t Bucket(KeyT key) const {
-    std::hash<KeyT> hash;
-
-    return (buckets_.empty() ? 0 : hash(key) % buckets_.size());
   }
 
   [[nodiscard]] size_t Size() const {
     return n_elements_;
   }
 
+  [[nodiscard]] size_t BucketCount() const {
+    return buckets_.size();
+  }
+  [[nodiscard]] size_t BucketSize(size_t id) const {
+    size_t result = 0;
+
+    if (id < buckets_.size()) {
+      auto& bucket = buckets_[id];
+      result = static_cast<size_t>(std::distance(bucket.begin(), bucket.end()));
+    }
+
+    return result;
+  }
+  [[nodiscard]] size_t Bucket(const KeyT& elem) const {
+    return (buckets_.empty() ? 0 : kHash(elem) % buckets_.size());
+  }
+
   [[nodiscard]] float LoadFactor() const {
-    return (this->Empty() ? 0 : static_cast<float>(n_elements_) / static_cast<float>(buckets_.size()));
+    return (buckets_.empty() ? 0 : static_cast<float>(n_elements_) / static_cast<float>(buckets_.size()));
   }
 
   [[nodiscard]] bool Empty() const {
@@ -78,108 +75,132 @@ class UnorderedSet {
   }
 
   void Clear() {
-    buckets_.clear();
     n_elements_ = 0;
+    buckets_.clear();
   }
 
-  bool Find(const KeyT& value) const {
-    bool answer = false;
-
-    if (!buckets_.empty()) {
-      auto& bucket = buckets_[Bucket(value)];
-
-      for (auto it = bucket.begin(); it != bucket.end() && !answer; ++it) {
-        answer |= (value == *it);
-      }
+  void Insert(const KeyT& elem) {
+    if (buckets_.empty()) {
+      buckets_.resize(1);
     }
 
-    return answer;
+    auto position = GetPosition(elem);
+    auto next_it = position.pre_iterator_;
+    ++next_it;
+
+    if (next_it == position.bucket_.end()) {
+      position.bucket_.insert_after(position.bucket_.before_begin(), elem);
+      ++n_elements_;
+    }
+
+    if (LoadFactor() > 1.0f) {
+      Reserve(2 * buckets_.size());
+    }
   }
 
-  void Insert(const KeyT& value) {
-    if (n_elements_ == buckets_.size()) {
-      this->Reserve((buckets_.empty() ? 1 : 2 * buckets_.size()));
+  [[nodiscard]] bool Find(const KeyT& elem) const {
+    auto result = false;
+
+    if (!Empty()) {
+      auto position = GetPosition(elem);
+      result = ++position.pre_iterator_ != position.bucket_.end();
     }
 
-    if (!this->Find(value)) {
-      auto& bucket = buckets_[Bucket(value)];
-
-      bucket.insert_after(bucket.before_begin(), value);
-    }
-
-    ++n_elements_;
+    return result;
   }
 
-  void Erase(const KeyT& value) {
-    if (!buckets_.empty()) {
-      auto& bucket = buckets_[Bucket(value)];
-      auto before_it = bucket.before_begin();
-      auto it = bucket.begin();
+  void Erase(const KeyT& elem) {
+    auto position = GetPosition(elem);
+    auto next_it = position.pre_iterator_;
+    ++next_it;
 
-      for (; it != bucket.end(); ++it, ++before_it) {
-        if (*it == value) {
-          break;
-        }
-      }
-
-      if (it != bucket.end()) {
-        bucket.erase_after(before_it);
-        --n_elements_;
-      }
+    if (next_it != position.bucket_.end()) {
+      position.bucket_.erase_after(position.pre_iterator_);
+      --n_elements_;
     }
   }
 
   void Reserve(size_t new_bucket_count) {
-    if (buckets_.size() < new_bucket_count) {
+    if (new_bucket_count > buckets_.size()) {
       buckets_.resize(new_bucket_count);
-      for (size_t bucket_idx = 0; bucket_idx < buckets_.size(); ++bucket_idx) {
-        auto it = buckets_[bucket_idx].before_begin();
-        auto real_it = it;
-        ++real_it;
-        while (real_it != buckets_[bucket_idx].end()) {
-          auto right_idx = Bucket(*(real_it));
-          if (right_idx == bucket_idx) {
-            ++it;
-            ++real_it;
+
+      for (size_t src_list_idx = 0; src_list_idx < buckets_.size(); ++src_list_idx) {
+        auto next_it = ++buckets_[src_list_idx].before_begin();
+
+        for (auto it = buckets_[src_list_idx].before_begin(); next_it != buckets_[src_list_idx].end();) {
+          auto idx = Bucket(*next_it);
+
+          if (idx != src_list_idx) {
+            buckets_[idx].splice_after(buckets_[idx].before_begin(), buckets_[src_list_idx], it);
+            next_it = it;
+            ++next_it;
           } else {
-            buckets_[right_idx].splice_after(buckets_[right_idx].before_begin(), buckets_[bucket_idx], it);
-            real_it = it;
-            ++real_it;
+            ++it;
+            ++next_it;
           }
         }
       }
     }
   }
-
   void Rehash(size_t new_bucket_count) {
-    if (new_bucket_count > buckets_.size() && new_bucket_count >= n_elements_) {
-      this->Reserve(new_bucket_count);
-    } else if (new_bucket_count < buckets_.size() & new_bucket_count >= n_elements_) {
-      for (size_t bucket_idx = 0; bucket_idx < buckets_.size(); ++bucket_idx) {
-        auto it = buckets_[bucket_idx].before_begin();
-        auto real_it = it;
-        ++real_it;
-        while (real_it != buckets_[bucket_idx].end()) {
-          std::hash<KeyT> hash;
-          auto right_idx = hash(*(real_it)) % new_bucket_count;
-          if (right_idx == bucket_idx) {
-            ++it;
-            ++real_it;
-          } else {
-            buckets_[right_idx].splice_after(buckets_[right_idx].before_begin(), buckets_[bucket_idx], it);
-            real_it = it;
-            ++real_it;
+    if (new_bucket_count != buckets_.size() &&
+        (static_cast<float>(n_elements_) / static_cast<float>(new_bucket_count)) <= 1.0f) {
+      if (new_bucket_count > buckets_.size()) {
+        Reserve(new_bucket_count);
+      } else {
+        for (size_t src_list_idx = 0; src_list_idx < buckets_.size(); ++src_list_idx) {
+          auto next_it = ++buckets_[src_list_idx].before_begin();
+          for (auto it = buckets_[src_list_idx].before_begin(); next_it != buckets_[src_list_idx].end();) {
+            auto idx = kHash(*next_it) % new_bucket_count;
+
+            if (idx != src_list_idx) {
+              buckets_[idx].splice_after(buckets_[idx].before_begin(), buckets_[src_list_idx], it);
+            } else {
+              ++it;
+              ++next_it;
+            }
           }
         }
-      }
-      auto delta = buckets_.size() - new_bucket_count;
-      for (size_t i = 0; i < delta; ++i) {
-        buckets_.pop_back();
+
+        buckets_.resize(new_bucket_count);
+        buckets_.shrink_to_fit();
       }
     }
   }
 
  private:
-  std::vector<std::forward_list<KeyT>> buckets_;
+  inline Position<KeyT, false> GetPosition(const KeyT& elem) {
+    auto idx = Bucket(elem);
+    auto& bucket = buckets_[Bucket(elem)];
+    auto it = bucket.before_begin();
+    auto next_it = it;
+    ++next_it;
+
+    for (; next_it != bucket.end(); ++it, ++next_it) {
+      if (*next_it == elem) {
+        break;
+      }
+    }
+
+    return {bucket, it};
+  }
+  inline Position<KeyT, true> GetPosition(const KeyT& elem) const {
+    auto& bucket = buckets_[Bucket(elem)];
+    auto it = bucket.before_begin();
+    auto next_it = it;
+    ++next_it;
+
+    for (; next_it != bucket.end(); ++it, ++next_it) {
+      if (*next_it == elem) {
+        break;
+      }
+    }
+
+    return {bucket, it};
+  }
+
   size_t n_elements_;
+  static constexpr std::hash<KeyT> kHash{};
+
+  std::vector<std::forward_list<KeyT>> buckets_;
 };
