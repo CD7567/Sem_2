b55ab4bcab664327f9e3f2c62f5afa8f17951384
cd7567 <alexander.y.koshelev@gmail.com>
2022-05-22 23:42:29 +0300

    checkpoint

--- main.cpp
+++ main.cpp
@@ -36,7 +36,6 @@ class CartesianTree {
   void Insert(int64_t k) {
     root_ = Merge(root_, new Node(k));
   }
-
   void Rotate(int64_t left, int64_t right) {
     auto split_1 = Split(root_, right);
     auto split_2 = Split(split_1.first, left - 1);
@@ -47,8 +46,7 @@ class CartesianTree {
 
     root_ = Merge(split_3.first, Merge(split_2.second, split_3.second));
   }
-
-  void Inorder() const {
+  void PrintInOrder() const {
     root_->HelpInorder();
   }
 
@@ -58,64 +56,9 @@ class CartesianTree {
   static int64_t Size(NodePtr node) {
     return (node != nullptr ? node->size_ : 0);
   }
-
   static void FixNode(NodePtr node) {
     node->size_ = (node != nullptr ? 1 + Size(node->left_child_) + Size(node->right_child_) : node->size_);
   }
-
-  NodePtr Merge(NodePtr tree_1, Node* tree_2) {
-    if (tree_1 == nullptr) {
-      return tree_2;
-    }
-
-    if (tree_2 == nullptr) {
-      return tree_1;
-    }
-
-    if (tree_1->priority_ > tree_2->priority_) {
-      tree_1->right_child_ = Merge(tree_1->right_child_, tree_2);
-      tree_1->right_child_->parent_ = tree_1;
-      FixNode(tree_1);
-      return tree_1;
-    }
-
-    tree_2->left_child_ = Merge(tree_1, tree_2->left_child_);
-    tree_2->left_child_->parent_ = tree_2;
-    FixNode(tree_2);
-    return tree_2;
-  }
-
-  std::pair<NodePtr, NodePtr> Split(NodePtr tree, int64_t x) {
-    if (tree == nullptr) {
-      return {nullptr, nullptr};
-    }
-
-    std::pair<NodePtr, NodePtr> pair;
-
-    if (Size(tree->left_child_) < x) {
-      pair = Split(tree->right_child_, x - Size(tree->left_child_) - 1);
-      tree->right_child_ = pair.first;
-      if (pair.first != nullptr) {
-        pair.first->parent_ = tree;
-      }
-      if (pair.second != nullptr) {
-        pair.second->parent_ = nullptr;
-      }
-      FixNode(tree);
-      return std::make_pair(tree, pair.second);
-    }
-    pair = Split(tree->left_child_, x);
-    tree->left_child_ = pair.second;
-    if (pair.second != nullptr) {
-      pair.second->parent_ = tree;
-    }
-    if (pair.first != nullptr) {
-      pair.first->parent_ = nullptr;
-    }
-    FixNode(tree);
-    return std::make_pair(pair.first, tree);
-  }
-
   static void Delete(NodePtr node) {
     if (node != nullptr) {
       Delete(node->left_child_);
@@ -123,6 +66,70 @@ class CartesianTree {
     }
 
     delete node;
+  }
+
+  static NodePtr Merge(NodePtr lhs_root, NodePtr rhs_root) {
+    NodePtr result;
+
+    if (lhs_root == nullptr) {
+      result = rhs_root;
+    } else if (rhs_root == nullptr) {
+      result = lhs_root;
+    } else {
+      if (lhs_root->priority_ > rhs_root->priority_) {
+        lhs_root->right_child_ = Merge(lhs_root->right_child_, rhs_root);
+        lhs_root->right_child_->parent_ = lhs_root;
+
+        result = lhs_root;
+      } else {
+        rhs_root->left_child_ = Merge(lhs_root, rhs_root->left_child_);
+        rhs_root->left_child_->parent_ = rhs_root;
+
+        result = rhs_root;
+      }
+    }
+
+    FixNode(result);
+    return result;
+  }
+  std::pair<NodePtr, NodePtr> Split(NodePtr root, int64_t value) {
+    std::pair<NodePtr, NodePtr> result{nullptr, nullptr};
+
+    if (root != nullptr) {
+      std::pair<NodePtr, NodePtr> pair;
+
+      if (Size(root->left_child_) < value) {
+        pair = Split(root->right_child_, value - Size(root->left_child_) - 1);
+        root->right_child_ = pair.first;
+
+        if (pair.first != nullptr) {
+          pair.first->parent_ = root;
+        }
+
+        if (pair.second != nullptr) {
+          pair.second->parent_ = nullptr;
+        }
+
+        result = std::make_pair(root, pair.second);
+      } else {
+        pair = Split(root->left_child_, value);
+        root->left_child_ = pair.second;
+
+        if (pair.second != nullptr) {
+          pair.second->parent_ = root;
+        }
+
+        if (pair.first != nullptr) {
+          pair.first->parent_ = nullptr;
+        }
+
+        result = std::make_pair(pair.first, root);
+      }
+
+      FixNode(root);
+    }
+
+    return result;
   }
 };
 
@@ -141,5 +148,5 @@ int main() {
     my_tree.Rotate(left_bound, right_bound);
   }
 
-  my_tree.Inorder();
+  my_tree.PrintInOrder();
 }
